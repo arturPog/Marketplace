fe6ae39d7a5324b20c607b9a1bf6ee895b79df77
ArtPog72 <artur@goldlab.pl>
2023-09-21 08:24:19 +0200

    Początek

---
+++ Marketplace.Domain/ClassifiedAd.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Marketplace.Domain
+{
+    public class ClassifiedAd
+    {
+        public Guid Id { get; set; }
+        public ClassifiedAd(Guid id) 
+        {
+            if (id == default) 
+            {
+                throw new ArgumentNullException("tożsamość musi być określona",nameof(id));
+            }
+            Id = id;
+
+        }
+        public void SetTitle(string title) => _title = title;
+        public void UpdateText(string text) => _text = text;
+        public void UpdatePrice(decimal price) => _price = price;
+
+        private Guid _ownerId;
+        private string _title;
+        private string _text;
+        private decimal _price;
+    }
+}
